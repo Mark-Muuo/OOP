b341799fd03ec2f1398f35903990ed52fa9068bd
Mark-Muuo <134997241+Mark-Muuo@users.noreply.github.com>
2023-10-18 21:09:01 +0300

    Add files via upload

--- BookInputTest.java
+++ BookInputTest.java
@@ -5,18 +5,13 @@ import java.util.Scanner;
 	public static void main(String[] args) {
 		book b = new book();
 		Scanner input=new Scanner(System.in);
-		String x,y;
-		int a;
+		
 		System.out.println("Please enter the title of the book");
-		x=input.nextLine();
+		b.title=input.nextLine();
 		System.out.println("Please enter the author name");
-		y=input.nextLine();
+		b.authour=input.nextLine();
 		System.out.println("Please enter the number of page");
-		a=input.nextInt();
-		b.title=""+x;
-        b.authour=""+y;
-        b.numberofpages=+a;
-	
+		b.numberofpages=input.nextInt();
         System.out.println("The book title is:"+b.title);
         System.out.println("The book author is:"+b.authour);
         System.out.println("The book has "+b.numberofpages+" pages");
